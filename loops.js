1be45b0f3d87298522e8682ba7b5c5fbfc3d28fc
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-15 17:33:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,12 @@
   var array = [];
 function forLoop(array) {
 
-  for (var i = 0; array <26; i++){
-    array[i] = "This is [i].";
+  for (let i = 0; i < 25; i++){
+    if (i ===1){
+      array[i] = `I am ${i} strange loop.`;
+    } else {
+      array[i] = `I am ${i} strange loops.`;
+    }
+    
   }
 }
